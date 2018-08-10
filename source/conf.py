543e2b383470c11a12f6fcb7b44c084f23936d28
mori0711 <mori-kazuki-mp@ynu.jp>
2018-08-11 03:29:19 +0900

    sds

--- source/conf.py
+++ source/conf.py
@@ -82,7 +82,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'basic'
+html_theme = 'bizstyle'
 #html_theme = 'pyramid'
 
 # Theme options are theme-specific and customize the look and feel of a theme
