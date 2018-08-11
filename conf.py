39226f06dcc1a17e44efc0e4024e89b9ef03ac7d
mori0711 <mori-kazuki-mp@ynu.jp>
2018-08-11 18:14:45 +0900

    dd

--- conf.py
+++ conf.py
@@ -21,6 +21,7 @@ CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('exts'))
 
 # -- General configuration -----------------------------------------------------
 
@@ -29,7 +30,7 @@ CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.todo', 
+extensions = ['sphinx.ext.todo',
               'sphinxcontrib.youtube',
               'sphinxcontrib.blockdiag'
               ]
--- index.rst
+++ index.rst
@@ -8,6 +8,7 @@ Exabyte.ioとは
 `EXabyte.io <https://exabyte.io/>`_
 
 .. youtube:: IN9YE_HjQQk
+.. youtube:: ecOZrN8_a3Y
 
 内容
 ====
